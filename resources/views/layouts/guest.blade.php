bd48889b96f0e295518b7e32f677353d9238e231
numanabid27 <numanabid+27@gmail.com>
2026-01-20 11:39:05 +0400

    remove test case

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
     <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
